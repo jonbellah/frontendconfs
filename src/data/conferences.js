1e79a126a30b9b0e6749f7397a56794d7df488aa
Jon Bellah <jon@ted.com>
2017-11-16 22:03:21 -0700

    Initial commit

---
+++ src/data/conferences.js
@@ -0,0 +1,7 @@
+import confs2017 from './2017';
+import confs2018 from './2018';
+
+const emptyArray = [];
+const conferences = emptyArray.concat(confs2017).concat(confs2018);
+
+export default conferences;
