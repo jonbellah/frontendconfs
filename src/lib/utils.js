e71786a675545dd360ef3a6b547db8a4c943424f
Jon Bellah <jon@ted.com>
2017-11-17 21:40:05 -0700

    Clean up date helper, add beyond tellerrand

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,24 @@
 function getDates(startDate, endDate) {
-  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const start = new Date(startDate);
   const end = new Date(endDate);
 
-  if (start.getDay() === end.getDay()) {
+  // Only one day
+  if (start.getDate() === end.getDate()) {
     return startDate;
   }
 
+  // If all conference days fall within the same month
   if (start.getMonth() === end.getMonth()) {
-    return `${monthNames[start.getMonth()]} ${start.getDay()}-${end.getDay()}, ${end.getFullYear()}`;
+    return `${monthNames[start.getMonth()]} ${start.getDate()}-${end.getDate()}, ${end.getFullYear()}`;
   }
 
-  return 'test';
+  // If all conference days fall within the same year
+  if (start.getFullYear() === end.getFullYear()) {
+    return `${monthNames[start.getMonth()]} ${start.getDate()} - ${monthNames[end.getMonth()]} ${end.getDate()}, ${end.getFullYear()}`;
+  }
+
+  return false;
 }
 
 export { getDates };
