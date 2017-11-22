d220ae68f674e38df77d9ab11454e24101b445d2
Jon Bellah <jon@ted.com>
2017-11-21 22:03:52 -0700

    Update favicon, footer styles

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer id="colophon">
-    footer
+  <footer id="colophon" className="p:6">
+    <div className="container d:f j-c:s-b a-i:c">
+      <span>Made in Colorado.</span>
+      <div className="d:f a-i:c">
+        <img src="https://s.gravatar.com/avatar/9c152b1584187e23aa5d9b1dc2f697b7?s=30" alt="Jon Bellah" className="round m-r:.5" />
+        <a href="https://jonbellah.com" className="d:f a-i:c">Jon Bellah</a>
+      </div>
+    </div>
   </footer>
 );
 
